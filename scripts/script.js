e59dfbfb3591d7ab3b8ec3a063bba8a64b9e2079
Alec Barnard <alecbarnard@live.com>
2020-08-14 10:24:56 -0700

    Add stripData

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
 const submitForm = (event) => {
-    console.log("Form received.")
-
     // Get values
-    const email = document.getElementById("email").value
-    const message = document.getElementById("message").value
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
 
-    console.log(message)
+    {[email, message] = stripData(email, message)}
 
     // Fetch config
     const url = "https://prod-23.eastus.logic.azure.com:443/workflows/f6edcfe87b164b248a12dcb7fdd58e02/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=w8BaMhGYnl14Yw8uF4n8vRhHZXNsiXLe2qxcQKVbnPk"
@@ -16,21 +14,32 @@ const submitForm = (event) => {
         },
         body: JSON.stringify({"email": email, "message": message})
     }
-
     // POST data
-    fetch(url, data)
+    //fetch(url, data)
 
     hideForm()
-
-
 }
 
 const hideForm = () => {
     const form = document.getElementById("form")
     const success = document.getElementById("success-form")
-    console.log("Hide form")
 
     form.style.display = "none"
     success.style.display = "block"
+}
+
+const stripData = (email, message) => {
+    // Trim white space
+    email = email.trim()
+    message = message.trim()
+    return [email, message]
+}
+
+const validateData = (email, message) => {
+    // Should return true or false for validate data
+
+
+    // Check for empty message 
+
 
 }
